ed9d49eb2fd1f1fae217f66bd0d2a024b5f92158
Paavo <pparkkin@gmail.com>
2017-06-06 21:42:47 +0800

    First steps towards IRC protocol handling.

--- src/Kolo.hs
+++ src/Kolo.hs
@@ -10,6 +10,7 @@ module Kolo
 import qualified Control.Concurrent as CC
 import qualified Control.Concurrent.MVar as MV
 import qualified Control.Concurrent.STM.TChan as TC
+import qualified Control.Lens as L
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C8
 import qualified Data.Conduit as C
@@ -18,6 +19,7 @@ import qualified Data.Conduit.Network as CN
 import qualified Data.Conduit.Network.TLS as TLS
 import qualified Data.UUID as UUID
 import qualified Data.UUID.V4 as UUID4
+import qualified Network.IRC.Conduit as IRCC
 import qualified Network.Simple.TCP as NS
 
 import Control.Monad.IO.Class ( liftIO )
@@ -32,7 +34,7 @@ data ServerConfig = ServerConfig
 
 data ServerState = ServerState
   { config :: ServerConfig
-  , users :: [(UUID.UUID, (TC.TChan BS.ByteString, CC.ThreadId))]
+  , users :: [(UUID.UUID, (TC.TChan IRCC.IrcMessage, CC.ThreadId))]
   }
 
 type Server = MV.MVar ServerState
@@ -43,40 +45,48 @@ defaultConfig = ServerConfig "*" 7779 "server.crt" "server.key"
 newServer :: ServerConfig -> IO Server
 newServer cfg = MV.newMVar (ServerState cfg [])
 
-addUser :: Server -> UUID.UUID -> TC.TChan BS.ByteString -> CC.ThreadId -> IO ()
+addUser :: Server -> UUID.UUID -> TC.TChan IRCC.IrcMessage -> CC.ThreadId -> IO ()
 addUser s uuid chan tid = do
   MV.modifyMVar_ s $ (\ss -> do
       return ss { users = (uuid, (chan, tid)) : users ss }
     )
 
-newUserChan :: IO (TC.TChan BS.ByteString)
+newUserChan :: IO (TC.TChan IRCC.IrcMessage)
 newUserChan = atomically $ TC.newTChan
 
-writeUserChan :: TC.TChan BS.ByteString -> BS.ByteString -> IO ()
+writeUserChan :: TC.TChan IRCC.IrcMessage -> IRCC.IrcMessage -> IO ()
 writeUserChan chan v = atomically $ TC.writeTChan chan v
 
-readUserChan :: TC.TChan BS.ByteString -> IO BS.ByteString
+readUserChan :: TC.TChan IRCC.IrcMessage -> IO IRCC.IrcMessage
 readUserChan chan = atomically $ TC.readTChan chan
 
-runUserThread :: TC.TChan BS.ByteString -> C.Consumer BS.ByteString IO () -> IO CC.ThreadId
-runUserThread chan sink = CC.forkIO $ C.connect (chanReader chan) sink
+runUserThread :: TC.TChan IRCC.IrcMessage -> C.Consumer BS.ByteString IO () -> IO CC.ThreadId
+runUserThread chan sink = CC.forkIO $ (chanReader chan) C.=$= IRCC.ircEncoder C.$$ sink
 
-chanWriter :: TC.TChan BS.ByteString -> C.Consumer BS.ByteString IO ()
+chanWriter :: TC.TChan IRCC.IrcMessage -> C.Consumer IRCC.IrcMessage IO ()
 chanWriter chan = CCo.mapM_ (writeUserChan chan)
 
-chanReader :: TC.TChan BS.ByteString -> C.Producer IO BS.ByteString
+chanReader :: TC.TChan IRCC.IrcMessage -> C.Producer IO IRCC.IrcMessage
 chanReader chan = CCo.repeatM (readUserChan chan)
 
-listen :: C.Producer IO BS.ByteString -> TC.TChan BS.ByteString -> IO ()
-listen source chan = C.connect source (chanWriter chan)
+extractMessage :: Either BS.ByteString IRCC.IrcEvent -> IRCC.IrcMessage
+extractMessage (Left e) = IRCC.RawMsg (BS.append (C8.pack "unrecognized ") e)
+extractMessage (Right e) = L.view IRCC.message e
 
-serverWelcome :: BS.ByteString -> BS.ByteString
+ircEventHandler :: C.Conduit (Either BS.ByteString IRCC.IrcEvent) IO IRCC.IrcMessage
+ircEventHandler = CCo.map extractMessage
+
+listen :: C.Producer IO BS.ByteString -> TC.TChan IRCC.IrcMessage -> IO ()
+listen source chan = source C.=$= IRCC.ircDecoder C.=$= ircEventHandler C.$$ (chanWriter chan)
+
+serverWelcome :: BS.ByteString -> IRCC.IrcMessage
 serverWelcome user =
-  foldr BS.append BS.empty
-    [ C8.pack "Welcome, "
-    , user
-    , C8.pack "!\n"
-    ]
+  IRCC.RawMsg $
+    foldr BS.append BS.empty
+      [ C8.pack "Welcome, "
+      , user
+      , C8.pack "!\n"
+      ]
 
 runListener :: Server -> CN.AppData -> IO ()
 runListener s a = do
